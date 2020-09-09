8acc803e5aa296cc1a215d722a61ca9df316d702
Alejandra2431 <69223047+Alejandra2431@users.noreply.github.com>
2020-09-09 11:04:20 -0300

    Ejercicio4
    Se agrega ejercicio 4

---
+++ Ejercicio_4/Ejercicio 4/src/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PII_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Archivo primer = new Archivo("../../nuevo.txt");
+            
+            Tablero tablero = new Tablero(primer.Board);
+            Imprimir.MostrarTablero(primer.Board);
+
+        }
+    }
+}
